22d3d65b3c782ad92ccfa9361bfcd6141dfee1b3
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-09 11:19:53 -0700

    added Forgot Password link and signin bar and button to SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -54,7 +54,20 @@ function SignIn() {
               onClick={() => setShowPassword((prevState) => !prevState)}
             />
           </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
         </form>
+
+        {/* Google OAuth component */}
       </div>
     </>
   );
