061c8f04ec7e26b637db234f23b0d14b6be864a6
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-11 19:45:55 -0700

    PrivateRoute component created to route to My Profile once a user is loggedin status is confirmed

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
 
 const PrivateRoute = () => {
-  const loggedIn = false;
+  // const loggedIn = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <h3>Loading...</h3>;
+  }
 
   // Outlet allows child element to return
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  // loading status
+  const [checkingStatus, setChekingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setChekingStatus(false);
+    });
+  });
+
+  return { loggedIn, checkingStatus };
+};
+
+// Protected routes in v6
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
