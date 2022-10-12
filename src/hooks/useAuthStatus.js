e67da3832f19e866cc1dd3e04a59dbb2d4778bd0
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-11 20:08:16 -0700

    In useAuthStatus hook, useRef (isMounted) is added to useEffect to fix errors

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,20 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   // loading status
   const [checkingStatus, setChekingStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setChekingStatus(false);
-    });
-  });
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setChekingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]); // dependency
 
   return { loggedIn, checkingStatus };
 };
