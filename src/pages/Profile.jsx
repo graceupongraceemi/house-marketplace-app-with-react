c2083de8d986f1681c43ada417ba7cc881286037
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-22 12:48:55 -0700

    In the Category page, fetch listings from firebase added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase.config';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 // eslint-disable-next-line
