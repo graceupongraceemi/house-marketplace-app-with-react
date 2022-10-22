452078fc01ffc3b7b3b074004cacd6f4c63b271f
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-21 21:50:59 -0700

    button with googleIcon added

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import googleIcon from '../assets/svg/googleIcon.svg';
 
 const OAuth = () => {
-  return <div>OAuth</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = () => {};
+
+  return (
+    <div className='socialLogin'>
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with</p>
+      <button className='socialIconDiv' onClick={onGoogleClick}>
+        <img className='socialIconImg' src={googleIcon} alt='google' />
+      </button>
+    </div>
+  );
 };
 
 export default OAuth;
