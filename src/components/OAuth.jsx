88c810ba2a1872a9e1d7d3fd635eb7aebf3ed9c0
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-21 22:29:20 -0700

    OAuth component created and Sign Up and Sign In with googleIcon optin completed

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import googleIcon from '../assets/svg/googleIcon.svg';
@@ -10,7 +10,30 @@ const OAuth = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const onGoogleClick = () => {};
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for user
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // If user, doesn't exist, create user
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        });
+      }
+      navigate('/');
+    } catch (error) {
+      toast.error('Could not authorize with Google');
+    }
+  };
 
   return (
     <div className='socialLogin'>
