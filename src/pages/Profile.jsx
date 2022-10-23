006461ecc412d1db84e34c5717eef29c2715b80d
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-22 19:26:20 -0700

    CreateListing page created and added into App.js route along with Link, arrowRight and homeIcon imported into the Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable */
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 // eslint-disable-next-line
 function Profile() {
@@ -99,6 +102,12 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
       </main>
     </div>
   );
